22869ce8a0f80e533030ae73601d1b93e89325e3
tkornel2000 <tkornel2000@gmail.com>
2023-01-14 21:22:03 +0100

    Add user to the database

--- src/main/java/kornel/todo/user/userController.java
+++ src/main/java/kornel/todo/user/userController.java
@@ -1,12 +1,21 @@
 package kornel.todo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class userController {
+    @Autowired private userRespoitory repo;
     @GetMapping("/addNewUser")
     public String addNewUser(){
         return "createUser";
+    }
+
+    @PostMapping("/succesfullyAdded")
+    public String save(user _user){
+        repo.save(_user);
+        return "succesfulTodo";
     }
 }
